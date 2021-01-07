233ca664604877e25934319ddd87095d60c11396
MCUmbrella <mcumbrella@protonmail.com>
2021-01-08 02:11:18 +0800

    a

--- src/vip/floatationdevice/mcumbrella/cslbungee/client/CSLBC.java
+++ src/vip/floatationdevice/mcumbrella/cslbungee/client/CSLBC.java
@@ -1,21 +1,31 @@
 package vip.floatationdevice.mcumbrella.cslbungee.client;
 
 import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
-import cc.baka9.catseedlogin.event.*;
 
-public class CSLBC extends JavaPlugin
+import cc.baka9.catseedlogin.event.CatSeedPlayerLoginEvent;
+
+public class CSLBC extends JavaPlugin implements Listener
 {
 	public static CSLBC main;
 	public void onEnable()
 	{
 		getLogger().info("Enabling.");
-		Bukkit.getPluginManager().registerEvents((Listener)CSLBCE.main, (Plugin)CSLBC.main);
+		Bukkit.getPluginManager().registerEvents(this, this);
 	}
 	public void onDisable()
 	{
 		getLogger().info("Disabled.");
 	}
+	@EventHandler
+	public void onLogin(CatSeedPlayerLoginEvent e)
+	{
+		if(e.getResult() == CatSeedPlayerLoginEvent.Result.SUCCESS)
+		{
+			getLogger().info(e.getPlayer().getName()+" login successful");
+		}
+	}
+
 }
