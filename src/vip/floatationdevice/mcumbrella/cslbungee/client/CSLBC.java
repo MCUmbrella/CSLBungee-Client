e15e8a4990139c9c3a405feeef6c1b03af12f541
MCUmbrella <mcumbrella@protonmail.com>
2021-01-08 01:44:41 +0800

    frame

--- src/vip/floatationdevice/mcumbrella/cslbungee/client/CSLBC.java
+++ src/vip/floatationdevice/mcumbrella/cslbungee/client/CSLBC.java
@@ -1,6 +1,21 @@
 package vip.floatationdevice.mcumbrella.cslbungee.client;
 
-public class CSLBC
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+import cc.baka9.catseedlogin.event.*;
+
+public class CSLBC extends JavaPlugin
 {
-	//
+	public static CSLBC main;
+	public void onEnable()
+	{
+		getLogger().info("Enabling.");
+		Bukkit.getPluginManager().registerEvents((Listener)CSLBCE.main, (Plugin)CSLBC.main);
+	}
+	public void onDisable()
+	{
+		getLogger().info("Disabled.");
+	}
 }
