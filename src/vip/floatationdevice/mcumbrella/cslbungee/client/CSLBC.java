c70d72f46e4b8fb8ba49239ac0f7a14db1998203
MCUmbrella <mcumbrella@protonmail.com>
2021-01-29 19:18:05 +0800

    Update CSLBC.java
    added operation on player registration

--- src/vip/floatationdevice/mcumbrella/cslbungee/client/CSLBC.java
+++ src/vip/floatationdevice/mcumbrella/cslbungee/client/CSLBC.java
@@ -8,19 +8,64 @@ import java.net.*;
 import java.io.*;
 
 import cc.baka9.catseedlogin.event.CatSeedPlayerLoginEvent;
+import cc.baka9.catseedlogin.event.CatSeedPlayerRegisterEvent;
 
 public class CSLBC extends JavaPlugin implements Listener
 {
-	public static CSLBC main;
 	int port=14514;
+	
+	public void upload(String name, String state)
+	{
+		new Thread("CSLBungee Client")
+		{
+			public void run()
+			{
+				for(short a=0;a!=5;a++)
+				{
+					try
+					{
+						Socket socket = new Socket();
+			            InetSocketAddress address=new InetSocketAddress("127.0.0.1", port);
+			            socket.connect(address,3000);
+			            OutputStreamWriter osw = new OutputStreamWriter(socket.getOutputStream(),"utf-8");
+			            osw.write("CSLBungee-Client-1.0\r\n"+state+"\r\n"+name);
+			            osw.flush();
+			            socket.shutdownOutput();
+			            osw.close();
+			            socket.close();
+			            getLogger().info("Successful uploaded");
+			            break;
+					}catch(Throwable ee)
+					{
+						if(a==4){
+							getLogger().warning("Failed to change login state of "+name+": ");
+							ee.printStackTrace();
+							break;
+						};
+					getLogger().warning("Error sending data. Retrying("+(a+1)+"/5) ("+ee.toString()+")");
+					continue;
+					}
+				}
+			}
+		}.start();
+
+	}
+	
 	public void onEnable()
 	{
-		getLogger().info("Player data will send to 127.0.0.1:14514");
 		Bukkit.getPluginManager().registerEvents(this, this);
+		getLogger().info("Player data will send to 127.0.0.1:14514");
 	}
 	public void onDisable()
 	{
 		getLogger().info("Disabled.");
+	}
+	
+	@EventHandler
+	public void onReg(CatSeedPlayerRegisterEvent e)
+	{
+		getLogger().info(e.getPlayer().getName()+" registered. Uploading to Bungee server");
+		upload(e.getPlayer().getName(), "S");
 	}
 	@EventHandler
 	public void onLogin(CatSeedPlayerLoginEvent e)
@@ -28,31 +73,7 @@ public class CSLBC extends JavaPlugin implements Listener
 		if(e.getResult() == CatSeedPlayerLoginEvent.Result.SUCCESS)
 		{
 			getLogger().info(e.getPlayer().getName()+" login successful. Uploading to Bungee server");
-			
-			new Thread("CSLBungee Client")
-			{
-				public void run()
-				{
-					for(short a=0;a!=5;a++)
-					{
-						try
-						{
-							Socket socket = new Socket();
-				            InetSocketAddress address=new InetSocketAddress("127.0.0.1", port);
-				            socket.connect(address,3000);
-				            OutputStreamWriter osw = new OutputStreamWriter(socket.getOutputStream(),"utf-8");
-				            osw.write("CSLBungee-Client-1.0\r\n"+"S\r\n"+e.getPlayer().getName());
-				            osw.flush();
-				            socket.shutdownOutput();
-				            osw.close();
-				            socket.close();
-				            getLogger().info("Successful uploaded");
-				            break;
-						}catch(Throwable ee){if(a==5){getLogger().warning("Failed to change login state of "+e.getPlayer().getName()+": ");ee.printStackTrace();break;};getLogger().warning("Error sending data. Retrying("+a+"/5) ("+ee.toString()+")");continue;}
-					}
-				}
-			}.start();
-			
+			upload(e.getPlayer().getName(), "S");
 		}
 	}
 
